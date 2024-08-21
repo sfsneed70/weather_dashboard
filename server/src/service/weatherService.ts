5215a1e39bc0eb076e10432a9f51a5b85be8690e
avca1998 <sfsneed@gmail.com>
2024-08-21 10:47:51 -0700

    updated 5day

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -79,7 +79,14 @@ class WeatherService {
   private parseCurrentWeather(response: any) {
     return new Weather(
       this.cityName || "",
-      new Date().toLocaleDateString(),
+      new Date().toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
       response.weather[0].icon,
       response.weather[0].description,
       response.main.temp,
